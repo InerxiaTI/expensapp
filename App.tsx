7d35161cf562324d69a246fcbed9e5d2f3e80c37
berracode <juanguillermoalarcon@gmail.com>
2023-07-29 15:53:09 -0500

    primer commit, se configura NativeWind

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'// App.jsx
+import { View, Text } from 'react-native'
+
+const App = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-neutral-900">
+      <Text className= "text-8xl text-gray-500">Open up App.js d to start working on your app!</Text>
+      
+    </View>
+  )
+}
+
+export default App
---
+++ README.md
@@ -0,0 +1,10 @@
+# Cuentas app o Expensapp
+
+## ejecutar 
+npx react-native run-android
+
+## Configuraciones:
+
+### Taildwind CSS
+https://www.nativewind.dev/quick-starts/react-native-cli
+
