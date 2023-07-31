030ab346666efb6e13b54a90824165df6c7b065e
berracode <juanguillermoalarcon@gmail.com>
2023-07-30 23:12:08 -0500

    se asignan bordes al tab bar bottom navigation

--- src/navigation/TabsNavigator.tsx
+++ src/navigation/TabsNavigator.tsx
@@ -16,14 +16,134 @@ const Tab2 = createBottomTabNavigator();
 
 export const TabsNavigator = () => {
   return (
+    TabNoMaterial2()
+  )
+}
+
+const TabMaterial = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName='Home'
+      inactiveColor='grey'
+      // style={{
+      //   borderRadius: 30
+      // }}
+
+      // barStyle={ {
+      //   borderTopWidth: 0,
+      //   backgroundColor: COLORS.tabNavigatorPrimaryColor,
+      //   borderRadius: 2,
+      //   borderColor: 'red',
+      //   borderWidth: 1
+      // }}
+
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ color, focused }) => {
+
+          let iconName: string = '';
+          switch (route.name) {
+            case 'HomeStack':
+              iconName = focused ? 'home' : 'home-outline'
+              break;
+
+            case 'NewShoppingList':
+              iconName = focused ? 'plus-circle' : 'plus-circle-outline'
+              break;
+
+            case 'Profile':
+              iconName = focused ? 'account' : 'account-outline'
+              break;
+          }
+
+          return <Icon name={iconName} size={24} color={color} />
+        },
+      })}
+
+    >
+      <Tab.Screen name="HomeStack"
+        options={{
+          tabBarLabel: 'Home',
+          tabBarColor: 'red'
+        }}
+        component={HomeStackNavigator} />
+      <Tab.Screen name="NewShoppingList" options={{ title: 'Nuevo' }} component={NewShoppingListScreen} />
+      <Tab.Screen name="Profile" options={{ tabBarLabel: 'Perfil' }} component={ProfileScreen} />
+    </Tab.Navigator>
+  )
+}
+
+const TabNoMaterial2 = () => {
+  return (
     <Tab2.Navigator
       initialRouteName='Home'
-      
+
       screenOptions={({ route }) => ({
         headerShown: false,
         tabBarActiveTintColor: 'white',
-        tabBarShowLabel: false,
-        tabBarStyle:{
+        // tabBarActiveBackgroundColor: COLORS.backgroudPrimary,
+        tabBarShowLabel: true,
+        tabBarStyle: {
+          borderTopWidth: 0,
+          backgroundColor: COLORS.tabNavigatorPrimaryColor,
+          // paddingTop: 7,
+          borderTopLeftRadius: 30,
+          borderTopRightRadius: 30,
+          borderLeftWidth: 0.2,
+          borderRightWidth: 0.2,
+          // borderColor: 'white',
+          position: 'absolute',
+          overflow: 'hidden',
+
+          shadowColor: "green",
+          shadowOffset: {
+            width: 0,
+            height: 6,
+          },
+          shadowOpacity: 0.37,
+          shadowRadius: 7.49,
+
+          elevation: 12,
+        },
+        tabBarIcon: ({ color, focused }) => {
+
+          let iconName: string = '';
+          switch (route.name) {
+            case 'HomeStack':
+              iconName = focused ? 'home' : 'home-outline'
+              break;
+
+            case 'NewShoppingList':
+              iconName = focused ? 'plus-circle' : 'plus-circle-outline'
+              break;
+
+            case 'Profile':
+              iconName = focused ? 'account' : 'account-outline'
+              break;
+          }
+
+          return <Icon name={iconName} size={24} color={color} />
+        },
+      })}
+
+    >
+      <Tab2.Screen name="HomeStack" options={{ title: 'Inicio' }} component={HomeStackNavigator} />
+      <Tab2.Screen name="NewShoppingList" options={{ title: 'Nuevo' }} component={NewShoppingListScreen} />
+      <Tab2.Screen name="Profile" options={{ tabBarLabel: 'Perfil' }} component={ProfileScreen} />
+    </Tab2.Navigator>
+  )
+}
+
+const TabNoMaterial = () => {
+  return (
+    <Tab2.Navigator
+      initialRouteName='Home'
+
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarActiveTintColor: 'white',
+        tabBarShowLabel: true,
+        tabBarStyle: {
           borderTopWidth: 0,
           backgroundColor: COLORS.tabNavigatorPrimaryColor,
         },
@@ -55,4 +175,3 @@ export const TabsNavigator = () => {
     </Tab2.Navigator>
   )
 }
-
