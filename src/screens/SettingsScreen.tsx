9549ef06b5264fcbdf36a681fbf04c8a2827f6dc
berracode <juanguillermoalarcon@gmail.com>
2023-09-22 18:46:35 -0500

    se agrega pantalla de settings

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,12 +1,15 @@
-import React, { useContext } from 'react'
-import { Text, TouchableOpacity, View } from 'react-native'
+import React, { useContext, useEffect } from 'react'
+import { StatusBar, Text, TouchableOpacity, View } from 'react-native'
 import BaseScreenComponent from '../components/BaseScreenComponent'
 import { AuthContext } from '../context/AuthContext';
 import { reset } from '../navigation/servicesUtil/NavigationService';
+import { ScrollView, TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+
 
 const SettingsScreen = () => {
 
-  const {logOut} = useContext(AuthContext);
+  const { logOut } = useContext(AuthContext);
 
   const handleLogOut = () => {
     logOut()
@@ -14,42 +17,83 @@ const SettingsScreen = () => {
 
   }
 
-  return (
-    <BaseScreenComponent
-    >
-        <Text className="text-blue-300 text-3xl">ProfileSSS screen</Text>
 
-        {/* Boton */}
-        <View
+
+  return (
+    <BaseScreenComponent screen='Settings' statusBarColor='#270C3D'>
+      <View
+        style={{
+          backgroundColor: '#270C3D',
+          height: 150
+        }}
+      >
+        <Text>Nombre usuario</Text>
+      </View>
+      <ScrollView
+        style={{
+          borderColor: 'yellow',
+          borderWidth: 0
+        }}
+      >
+        {/* Card settings */}
+        <TouchableOpacity
+          onPress={handleLogOut}
+          style={{
+            backgroundColor: '#262626',
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            height: 50,
+            borderBottomColor: 'black',
+            borderBottomWidth: 1,
+          }}
+        >
+          <View
             style={{
+              borderColor: 'red',
               borderWidth: 0,
-              borderColor: 'yellow',
-              marginHorizontal: 33,
-              marginTop: 12
+              flexDirection: 'row',
+              alignItems: 'center',
+              marginLeft: 15,
+              gap: 24
             }}
           >
-            <TouchableOpacity
-              onPress={() => handleLogOut()}
-              style={{
-                backgroundColor: '#7600D3',
-                borderRadius: 20,
-                height: 50,
-                justifyContent: 'center',
-                alignItems: 'center',
-                elevation: 3,
-              }}
-            >
+            {/* Icon */}
+            <View>
+              <Icon name="logout" size={25} color="white"/>
 
+            </View>
+
+            {/* Texto */}
+            <View>
               <Text
                 style={{
                   fontSize: 14,
-                  fontWeight: '700',
-                  color: 'white'
-
+                  color: 'white',
+                  fontStyle: 'normal',
+                  fontWeight: '400'
                 }}
               >Cerrar sesión</Text>
-            </TouchableOpacity>
+
+            </View>
           </View>
+
+          {/* Common icon */}
+          <View
+            style={{
+              marginRight: 12
+            }}
+          >
+              <Icon name="chevron-right" size={25} color="white"/>
+          </View>
+
+        </TouchableOpacity>
+
+
+      </ScrollView>
+
+
     </BaseScreenComponent>
   )
 }
